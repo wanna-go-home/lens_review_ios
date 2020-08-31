b9b69e397abcfecb36e09e3a29a36058e8e77f00
suning <suning@suningui-MacBook-Air.local>
2020-08-31 21:34:10 +0900

    Init commit

---
+++ lens_review_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lens_review_ios
+//
+//  Created by suning on 2020/08/31.
+//  Copyright © 2020 wannagohome. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
