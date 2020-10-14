40fae59a4f72b1d3072921205164a08b75268135
seonhee <suning7890@gmail.com>
2020-10-14 22:17:21 +0900

    Add tabview

--- lens_review_ios/ContentView.swift
+++ lens_review_ios/ContentView.swift
@@ -11,7 +11,30 @@ import SwiftUI
 struct ContentView: View
 {
     
-    @ObservedObject var lensViewModel: LensViewModel = LensViewModel()
+//    @ObservedObject var lensViewModel: LensViewModel = LensViewModel()
+    @EnvironmentObject var lensViewModel: LensViewModel
+
+    var body: some View {
+        TabView{
+            LensListView()
+            Text("2")
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("자유 게시판")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(LensViewModel())
+    }
+}
+
+struct LensListView: View {
+    
+    @EnvironmentObject var lensViewModel: LensViewModel
     
     var body: some View {
         NavigationView
@@ -28,12 +51,9 @@ struct ContentView: View
                 }
             }
             .navigationBarTitle(Text("Lens List"))
+        }.tabItem {
+            Image(systemName: "list.dash")
+            Text("렌즈 리스트")
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
