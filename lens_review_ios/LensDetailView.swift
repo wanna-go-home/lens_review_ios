59ffcf6949facb6754d3d4b0d994b63503998bac
seonhee <suning7890@gmail.com>
2020-11-08 22:13:30 +0900

    Lens Detail UI

--- lens_review_ios/LensDetailView.swift
+++ lens_review_ios/LensDetailView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct LensDetailView: View
 {
@@ -15,12 +16,11 @@ struct LensDetailView: View
     
     var body: some View {
         VStack{
-            Text(lensDetailViewModel.lens.name)
-            Text("\(lensDetailViewModel.lens.price)")
-        }.onAppear(perform: LensDetail)
+            LensDetailRow(lens_: lensDetailViewModel.lens)
+        }.onAppear(perform: callLensDetail)
     }
     
-    func LensDetail()
+    func callLensDetail()
     {
         lensDetailViewModel.getLensDetail(id: selectedLensId)
     }
@@ -29,5 +29,44 @@ struct LensDetailView: View
 struct LensDetailView_Previews: PreviewProvider {
     static var previews: some View {
         LensDetailView(selectedLensId: 1)
+    }
+}
+
+struct LensDetailRow: View {
+    
+    var lens_: LensDetail
+
+    var body: some View {
+        if !lens_.productImage.isEmpty {
+            URLImage(url: URL(string: lens_.productImage[0])!) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 240, height: 240)
+            }
+        }
+        Text("렌즈 브랜드")
+            .font(.system(size: 15))
+            .padding(.top, 12)
+        Text(lens_.name)
+            .font(.system(size: 18))
+            .fontWeight(.bold)
+            .padding([.top, .bottom], 10)
+        
+        // TODO : Text 상수 처리, spacing 조절
+        HStack(spacing: 1) {
+            Text("그래픽 직경: \(lens_.graphicDia, specifier: "%.1f")mm")
+                .font(.system(size: 13))
+            Text("| 가격: \(lens_.price)원")
+                .font(.system(size: 13))
+        }
+        
+        HStack(spacing: 1) {
+            Text("평점: ")
+                .font(.system(size: 13))
+            Text("4.15")
+                .font(.system(size: 13))
+                .foregroundColor(.purple)
+        }
     }
 }
