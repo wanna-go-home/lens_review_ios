a4556af5b320234bbcfb70fb2734f6ef3a8ae609
seonhee <suning7890@gmail.com>
2020-11-29 23:15:35 +0900

    FreeBoard UI(without like, view, reply button)

--- lens_review_ios/Network/Model/FreeBoardPreview.swift
+++ lens_review_ios/Network/Model/FreeBoardPreview.swift
@@ -13,21 +13,21 @@ struct FreeBoardPreview : Decodable, Identifiable
     var account : String = ""
     var nickname : String = ""
     var title : String = ""
-//    var content : String = ""
-//    var viewCnt : Int = 0
-//    var likeCnt : Int = 0
-//    var replyCnt : Int = 0
-//    var createdAt : String = ""
+    var content : String = ""
+    var viewCnt : Int = 0
+    var likeCnt : Int = 0
+    var replyCnt : Int = 0
+    var createdAt : String = ""
 
     enum CodingKeys: String, CodingKey {
         case id
         case account
         case nickname
         case title
-//        case content
-//        case viewCnt
-//        case likeCnt
-//        case replyCnt
-//        case createdAt = "created_at"
+        case content
+        case viewCnt
+        case likeCnt
+        case replyCnt
+        case createdAt
     }
 }
--- lens_review_ios/UI/FreeBoard/BoardListView.swift
+++ lens_review_ios/UI/FreeBoard/BoardListView.swift
@@ -14,18 +14,77 @@ struct BoardListView: View {
     var body: some View {
         NavigationView
         {
-            List
+            ScrollView
             {
-                ForEach(boardListViewModel.boardList) { board_ in
-                    HStack {
+                VStack(alignment: .leading)
+                {
+                    ForEach(boardListViewModel.boardList) { board_ in
+                        FreeBoardRow(board_: board_)
                         NavigationLink(destination: Text(board_.title))
                         {
-                            Text(board_.title)
+                            // NavigationLink 적용 시 화살표 안 뜨게 하기 위함
+                            EmptyView()
                         }
+                        .buttonStyle(PlainButtonStyle())
                     }
                 }
+                .frame(minHeight: 100)
             }
-            .navigationBarTitle(Text("Board List"))
+            .padding([.leading, .trailing])
+            .navigationBarHidden(true)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct FreeBoardRow: View {
+    
+    var board_: FreeBoardPreview
+    
+    // color 해야함
+    var body: some View {
+        VStack(alignment: .leading)
+        {
+            // 상단
+            VStack(alignment: .leading) {
+                Text("\(board_.title)")
+                    .font(.system(size: 18))
+                    .foregroundColor(.black)
+                    .padding(.top, 5)
+                Text("\(board_.content)")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color("BoardContentColor"))
+                    .padding(.top, 5)
+                Text("\(board_.nickname)")
+                    .font(.system(size: 11))
+                    .foregroundColor(.black)
+                    .padding(.top, 15)
+            }
+            .padding([.leading, .trailing], 12)
+            .padding(.top, 20)
+            
+            Divider()
+            
+            // 하단
+            HStack(spacing: 5) {
+                Text("\(board_.viewCnt)")
+                    .font(.system(size: 11))
+                Text("\(board_.likeCnt)")
+                    .font(.system(size: 11))
+                Text("\(board_.replyCnt)")
+                    .font(.system(size: 11))
+            }
+            .frame(height: 18)
+            .padding([.leading, .trailing], 12)
+            
+            ScrollDivider()
+        }
+    }
+}
+
+struct BoardListView_Previews: PreviewProvider {
+    static var previews: some View {
+//        BoardListView().environmentObject(BoardListViewModel())
+        FreeBoardRow(board_: FreeBoardPreview())
     }
 }
