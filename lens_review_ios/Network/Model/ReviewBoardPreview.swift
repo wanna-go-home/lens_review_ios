f9bf0d423080da2acfc1cee5b3c2faab4aa1bb22
seonhee <suning7890@gmail.com>
2020-12-13 00:56:50 +0900

    Review List UI

--- lens_review_ios/Network/Model/ReviewBoardPreview.swift
+++ lens_review_ios/Network/Model/ReviewBoardPreview.swift
@@ -14,9 +14,10 @@ struct ReviewBoardPreview : Decodable, Identifiable
     var nickname : String = ""
     var title : String = ""
     var content : String = ""
+    var viewCnt : Int = 0
     var likeCnt : Int = 0
     var replyCnt : Int = 0
-//    var createdAt : String = ""
+    var createdAt : String = ""
     var lensId : Int = 0
 
     enum CodingKeys: String, CodingKey {
@@ -25,9 +26,10 @@ struct ReviewBoardPreview : Decodable, Identifiable
         case nickname
         case title
         case content
+        case viewCnt
         case likeCnt
         case replyCnt
-//        case createdAt = "created_at"
+        case createdAt
         case lensId
     }
 }
