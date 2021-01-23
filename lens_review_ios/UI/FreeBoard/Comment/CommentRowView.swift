16beec4f2ed6f18bb198ae67852b524b83e2fdaa
seonhee-kim <suning7890@gmail.com>
2021-01-23 22:47:53 +0900

    Separation of comment / child-comment view

--- lens_review_ios/UI/FreeBoard/Comment/CommentRowView.swift
+++ lens_review_ios/UI/FreeBoard/Comment/CommentRowView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct CommentRowView: View
 {
+    @State private var showMoreAction = false
+    @State private var showMofifyView = false
+    @State private var showDeleteAlert = false
+    @State private var showReportView = false
+    
     var comment: FreeBoardComment
     var moreFlag : Bool
     
@@ -57,11 +62,18 @@ struct CommentRowView: View
                         
                         Spacer()
                         
-                        Image("more-hori")
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width:18, height: 18)
-                            .foregroundColor(Color("IconColor"))
-                            .padding(.trailing, 25)
+                        Button(action: {
+                            self.showMoreAction = true
+                        }, label: {
+                            Image("more-hori")
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:18, height: 18)
+                                .foregroundColor(Color("IconColor"))
+                                .padding(.trailing, 25)
+                        })
+                        .actionSheet(isPresented: $showMoreAction){
+                            commentActionSheet()
+                        }
                     }
                     .font(.system(size: 14))
                     .foregroundColor(Color("BoardContentColor"))
@@ -89,59 +101,26 @@ struct CommentRowView: View
             
             Divider()
         }
-    }
-}
-
-struct ChildCommentRowView: View
-{
-    var comment: FreeBoardComment
-    
-    var body: some View
-    {
-        VStack(alignment: .leading)
+        .alert(isPresented: $showDeleteAlert)
         {
-            VStack(alignment: .leading, spacing: 12)
-            {
-                Text(comment.nickname)
-                    .font(.system(size: 14))
-                    .foregroundColor(Color("BoardContentColor"))
-                    
-                Text(comment.content)
-                    .font(.system(size: 17))
-                    .foregroundColor(Color("BoardTitleColor"))
-                    
-                HStack(spacing: 20)
-                {
-                    Text(calcCreatedBefore(createdAt: comment.createdAt))
-                    
-                    HStack(spacing: 5)
-                    {
-                        Image("like")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width:18, height: 18)
-                            .foregroundColor(Color("IconColor"))
-                    
-                        Text("\(comment.likeCnt)")
-                    }
-                    
-                    Spacer()
-                    
-                    Image("more-hori")
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:18, height: 18)
-                        .foregroundColor(Color("IconColor"))
-                        .padding(.trailing, 25)
-                }
-                .font(.system(size: 14))
-                .foregroundColor(Color("BoardContentColor"))
-                .frame(height: 25)
-            }
-            .padding(.leading, 37)
-            .padding([.top, .bottom], 7)
-            
-            Divider()
+            Alert(title: Text(""), message: Text("delete_article_question".localized()),
+                  primaryButton: .destructive(Text("delete_button_title".localized()), action: { }),
+                  secondaryButton: .cancel())
         }
-        .background(Color("ChildCommentColor"))
+    }
+    
+    fileprivate func commentActionSheet() -> ActionSheet {
+        var commentButtons = [ActionSheet.Button]()
+        
+        if comment.isAuthor {
+            commentButtons.append(.default(Text("modify".localized()), action: { self.showMofifyView = true }))
+            commentButtons.append(.destructive(Text("delete".localized()), action: { self.showDeleteAlert = true }))
+        }else {
+            commentButtons.append(.destructive(Text("report".localized()), action: { self.showReportView = true }))
+        }
+        
+        commentButtons.append(.cancel())
+        
+        return ActionSheet(title: Text("article_comment_actionSheet_title".localized()), buttons: commentButtons)
     }
 }
