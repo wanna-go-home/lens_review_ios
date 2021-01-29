0e0de473179962fc4752d264e318e7bc1bbd0ec9
seonhee-kim <suning7890@gmail.com>
2021-01-29 16:52:23 +0900

    Add login error handling

--- lens_review_ios/UI/UIConstants.swift
+++ lens_review_ios/UI/UIConstants.swift
@@ -20,3 +20,9 @@ struct CommentRequestResult
     static let childSuccess = 1
     static let failure = 2
 }
+
+struct LoginErrType
+{
+    static let WrongInput = 1
+    static let ServerError = 2
+}
